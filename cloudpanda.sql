c4819e781277a0dd09b0daf7b89274b1e6f5d2c7
niraj_jani_ccc <niraj.jani@contentcreation.com.my>
2018-03-22 19:07:46 +0800

    sql file added

--- cloudpanda.sql
+++ cloudpanda.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 22, 2018 at 04:03 AM
--- Server version: 5.7.21-0ubuntu0.16.04.1
--- PHP Version: 7.0.22-0ubuntu0.16.04.1
+-- Host: localhost:3306
+-- Generation Time: Mar 22, 2018 at 07:06 PM
+-- Server version: 5.7.20-0ubuntu0.17.04.1
+-- PHP Version: 7.0.22-0ubuntu0.17.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -39,9 +39,13 @@ CREATE TABLE `organizations` (
 --
 
 INSERT INTO `organizations` (`id`, `name`, `position`, `parent`, `created_at`) VALUES
-(1, 'CP CEO Name', 'CEO', 0, '2018-03-21 16:38:07'),
-(2, 'CP Manager First', 'Manager', 1, '2018-03-21 16:38:07'),
-(3, 'CP Manager Second', 'Manager', 1, '2018-03-21 16:38:07');
+(1, 'CEO Name', 'CEO', 0, '2018-03-21 16:38:07'),
+(2, 'Manager First', 'QA Manager', 1, '2018-03-21 16:38:07'),
+(4, 'Manager Second', 'Development\n Manager', 1, '2018-03-21 16:38:07'),
+(6, 'Team Lead 1', 'Second Level', 2, '2018-03-22 07:45:27'),
+(7, 'Manager Third', 'Project Manager', 1, '2018-03-21 16:38:07'),
+(8, 'Manager Fourth', 'Content Manager', 1, '2018-03-21 16:38:07'),
+(9, 'Team Lead 2', 'Second Level', 2, '2018-03-22 07:45:27');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +65,7 @@ ALTER TABLE `organizations`
 -- AUTO_INCREMENT for table `organizations`
 --
 ALTER TABLE `organizations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
